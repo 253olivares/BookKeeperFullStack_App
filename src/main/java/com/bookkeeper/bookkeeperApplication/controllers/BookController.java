b7ea5abfd3b9822e002fb053a9f56cc28a11b046
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-10-03 12:43:02 -0400

    Set up controllers
    - Set up each controller by creating their annotations
    - Each controller has an annotation for Rest controller which allows our application to scan the class for endpoints.
    - Additionally to the rest controller annotation we also provide a request mapping annotation which allows our classes and methods to create a rest api path. This way we can have our post push put delete endpoints at specific end points.

--- src/main/java/com/bookkeeper/bookkeeperApplication/controllers/BookController.java
+++ src/main/java/com/bookkeeper/bookkeeperApplication/controllers/BookController.java
@@ -1,5 +1,18 @@
 package com.bookkeeper.bookkeeperApplication.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Out Rest controller is used to create a restful web service using Spring MVC
+// Using Rest Controller we can watch and listen for api calls
+@RestController
+
+// In SpringFramework The @RequestMapping annotation is used to map web requests
+// to a specific handler or classes.
+// this allows us to define url patterns, Http methods, and other parameters
+// in this case we are setting our root users api route
+@RequestMapping("/book")
+
 public class BookController {
 
 }
--- src/main/java/com/bookkeeper/bookkeeperApplication/controllers/ReviewsController.java
+++ src/main/java/com/bookkeeper/bookkeeperApplication/controllers/ReviewsController.java
@@ -1,5 +1,17 @@
 package com.bookkeeper.bookkeeperApplication.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Out Rest controller is used to create a restful web service using Spring MVC
+// Using Rest Controller we can watch and listen for api calls
+@RestController
+
+// In SpringFramework The @RequestMapping annotation is used to map web requests
+// to a specific handeler or classes.
+// this allows us to define url patterns, Http methods, and other parameters
+// in this case we are setting our root users api route
+@RequestMapping("/reviews")
 public class ReviewsController {
 
 }
